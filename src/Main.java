ad0c40520b457c9cdcc4f5851570258a75fdd367
Alexander <kuhar@mail.ru>
2022-11-05 17:55:11 +0300

    Выполнил дз "Удаление нечетных элементов"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Main {
 
+    static Set<Integer> number = new HashSet<>();
+
     public static void main(String[] args) {
-	// write your code here
+        for (Integer i = 0; i < 20; i++) {
+            number.add((int) Math.round(Math.random() * 1001));
+        }
+        System.out.println(number);
+        removeNumber((HashSet<Integer>) number);
+        System.out.println(number);
+    }
+
+    public static void removeNumber(HashSet<Integer> set) {
+        Iterator<Integer> numbers = set.iterator();
+        Integer i;
+        while (numbers.hasNext()) {
+            i = numbers.next();
+            if (i % 2 == 1) {
+                numbers.remove();
+            }
+        }
     }
 }
